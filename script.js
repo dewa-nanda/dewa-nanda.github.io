70a6377a4c62ce3b82ed1b6d3623ad3730639a41
Dewa Putra Hernanda <dewananda089@gmail.com>
2024-03-06 12:45:15 +0700

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.type-effect', {
+    strings: ['Student', 'Lifetime Learner' , 'Programmer', 'Web Developer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true,
+});
+
+var secTyped = new Typed('#greetings', {
+    strings: ['hallo'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true,
+});
